5b2c0efa5688d4f56e16efdaba33535ca63aeb25
mateivul <mateivultur08@gmail.com>
2026-03-30 19:40:14 +0300

    banner and about me

--- data.js
+++ data.js
@@ -3,11 +3,25 @@ const DATA = {
     username: "visitor",
     hostname: "portofolio",
 
-    banner: "",
+    banner: `
+ __  __       _       _  __     __     _ _              
+|  \/  | __ _| |_ ___(_) \ \   / /   _| | |_ _   _ _ __ 
+| |\/| |/ _\` | __/ _ \ |  \ \ / / | | | | __| | | | '__|
+| |  | | (_| | ||  __/ |   \ V /| |_| | | |_| |_| | |   
+|_|  |_|\__,_|\__\___|_|    \_/  \__,_|_|\__|\__,_|_|                                                         
+`,
 
     welcome: ["Welcome to my terminal portofolio!", "Type 'help' to see available commands.", ""],
 
-    about: "",
+    about: [
+        "Hi! I'm Matei Vultur",
+        "",
+        "I'm a high school student and a developer who loves building things",
+        "with code. I'm interested in web dev, physics",
+        "simulations, taycoon games and creative coding projects.",
+        "Currently exploring: React, Python(Manim), Canvas animations",
+        "Fun fact: I believe true complexity lies in simplicity!",
+    ],
 
     projects: [
         {
