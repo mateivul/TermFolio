774bcb36dae56a7c34b15de9e562570e001547c3
mateivul <mateivultur08@gmail.com>
2026-03-30 18:43:39 +0300

    data about me part 1

---
+++ data.js
@@ -0,0 +1,40 @@
+const DATA = {
+    name: "Matei Vultur",
+    username: "visitor",
+    hostname: "portofolio",
+
+    banner: "",
+
+    welcome: ["Welcome to my terminal portofolio!", "Type 'help' to see available commands.", ""],
+
+    about: "",
+
+    projects: [
+        {
+            name: "TermFolio",
+            emoji: "💻",
+            description: "Terminal-style protofolio website - the one you're using right now!",
+            tech: ["Vanilla JS", "HTML", "CSS"],
+            url: "https://github.com/mateivul/TermFolio",
+        },
+    ],
+
+    skills: {
+        languages: [],
+        frameworks: [],
+        tools: [],
+        learning: [],
+    },
+
+    contact: {
+        email: "mateivultur08@gmail.com",
+        locaiton: "Romania",
+    },
+
+    socials: {
+        github: "https://github.com/mateivul",
+        instagram: "https://www.instagram.com/matei.vul",
+    },
+
+    fortunes: [],
+};
